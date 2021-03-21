7fa4bff66673313699dbc190cc5526debe306d31
awaggoner1988 <81038434+awaggoner1988@users.noreply.github.com>
2021-03-21 17:40:35 -0500

    test commit 2

---
+++ app/src/main/java/com/example/animalapp/Common/ModelInfo.java
@@ -0,0 +1,16 @@
+package com.example.animalapp.Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.animalapp.R;
+
+public class ModelInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model_info);
+    }
+}
