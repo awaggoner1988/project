f369ebf9478b8515df394b4e6a4c16b302e00445
kamalkarkee <81118481+kamalkarkee@users.noreply.github.com>
2021-03-31 21:00:39 -0500

    update

--- app/src/main/java/com/example/animalapp/Common/UserProfile.java
+++ app/src/main/java/com/example/animalapp/Common/UserProfile.java
@@ -2,15 +2,29 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.animalapp.R;
 
  public class UserProfile extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        findViewById(R.id.buttonHome).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Dashboard.class));
+            }
+        });
     }
-}
+public void update(View view){
+
+    }
+
+ }
