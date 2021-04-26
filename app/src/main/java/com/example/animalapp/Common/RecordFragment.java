ed8af72fe2edcdc30aab0073fcc00fbeaa93b3ad
Asim Regmi <asim.regmi@gmail.com>
2021-04-26 01:02:04 -0500

    added record functionality

--- app/src/main/java/com/example/animalapp/Common/RecordFragment.java
+++ app/src/main/java/com/example/animalapp/Common/RecordFragment.java
@@ -1,21 +1,33 @@
 package com.example.animalapp.Common;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.SystemClock;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.animalapp.R;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,6 +42,14 @@ public class  RecordFragment extends Fragment implements View.OnClickListener {
     private ImageButton recordBtn;
 
     private boolean isRecording = false;
+
+    private String recordPermission = Manifest.permission.RECORD_AUDIO;
+    private int PERMISSION_CODE = 21;
+
+    private MediaRecorder mediaRecorder;
+    private String recordFile;
+
+    private Chronometer timer;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -85,6 +105,8 @@ public class  RecordFragment extends Fragment implements View.OnClickListener {
         listBtn = view.findViewById(R.id.record_list);
         recordBtn = view.findViewById(R.id.record_btn);
 
+        timer = view.findViewById(R.id.record_timer);
+
         listBtn.setOnClickListener(this);
         recordBtn.setOnClickListener(this);
     }
@@ -98,14 +120,64 @@ public class  RecordFragment extends Fragment implements View.OnClickListener {
             case R.id.record_btn:
                 if (isRecording) {
                     //Stop Recording
-                    recordBtn.setColorFilter(Color.argb(192,192,192,192));
+                    stopRecording();
+
+                    recordBtn.setColorFilter(Color.argb(255,255,255,255));
                     isRecording = false;
                 } else {
                     //Start Recording
-                    recordBtn.setColorFilter(Color.argb(255,255,255,255));
-                    isRecording = true;
+                    if (checkPermissions()) {
+
+                        startRecording();
+                        recordBtn.setColorFilter(Color.argb(192,192,192,192));
+                        isRecording = true;
+                    }
+
                 }
                 break;
+        }
+    }
+
+    private void startRecording() {
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.start();
+
+        String recordPath = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.US);
+        Date now = new Date();
+
+        recordFile = "Animalsound_" + formatter.format(now) + ".3gp" ;
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(recordPath + "/" + recordFile);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    private void stopRecording() {
+
+        timer.stop();
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(getContext(), recordPermission) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+
+        else {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {recordPermission}, PERMISSION_CODE);
+            return false;
         }
     }
 }
